c280df5ee484244a3c534e485cfe67ed0fb49c38
李元琦 <yuan.chi@program.com.tw>
2020-11-18 09:02:48 +0800

    test

--- routes/postback/stores.js
+++ routes/postback/stores.js
@@ -1,6 +1,7 @@
 const { client } = require('../../libs/lineat')
 
 module.exports = async ({ event, app }) => {
+  console.log(app.locals.storeCitys)
   const msg = require('../../views/stores/list')(app.locals.storeCitys)
   return client.replyMessage(event.replyToken, msg)
 }
