fedfd51556c03a634cf298621c61665e84d6dbc9
李元琦 <yuan.chi@program.com.tw>
2022-02-08 17:13:29 +0800

    調整影片排序

--- routes/postback/playItems.js
+++ routes/postback/playItems.js
@@ -6,7 +6,7 @@ module.exports = async ({ event, args }) => {
   const item = args[0]
   try {
     const playVideos = await GetData.PlayVideos()
-    const videos = _.orderBy(_.compact(_.map(item.videos, vId => playVideos[vId])), ['priority', 'category_en'], ['desc', 'asc'])
+    const videos = _.orderBy(_.compact(_.map(item.videos, vId => playVideos[vId])), 'level', 'asc')
     if (!videos.length) throw new Error('沒有教學影片')
     return client.replyMessage(event.replyToken, require('../../views/play/videos')({ item, videos }))
   } catch (err) {
