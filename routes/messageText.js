030bf43a50ac1730703be1216837e048adf67c48
李元琦 <yuan.chi@program.com.tw>
2022-02-11 17:52:21 +0800

    GA

--- routes/messageText.js
+++ routes/messageText.js
@@ -22,7 +22,7 @@ module.exports = async ({ event, app }) => {
   const items = await SearchPlayItemsByKeyword(text)
   if (items.length) return require('./postback/playListByKeyword')({ event, items, keyword: text })
   // 沒有此查詢資料
-  event.gaScreenView('查詢 LINE ID')
+  event.gaScreenView('未知訊息')
   event.gaEventLabel('未知訊息', '未知訊息', text)
   await client.replyMessage(event.replyToken, require('../views/notFound')(text))
 }
